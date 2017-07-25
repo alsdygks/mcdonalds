101f8221c050f5127f325d426ea674753b7b81f7
jinlee10 <jinleexjinlee@gmail.com>
2017-07-25 20:15:44 +0900

    new commit!

--- JSTeamProject/WebContent/employee.js
+++ JSTeamProject/WebContent/employee.js
@@ -1,7 +1,6 @@
 /**
  * 
  */
-
 function Employee(empNo,empName ,empAge ,empSal ,empSex ,empPart ,empPos ,empWorkHour){
  	this.empNo = empNo ,//사번
 	this.empName= empName , //이름
@@ -13,4 +12,21 @@ function Employee(empNo,empName ,empAge ,empSal ,empSex ,empPart ,empPos ,empWor
 	this.empWorkHour=empWorkHour//근무시간
 		
 }
+// getters n setters
+Employee.prototype.getEmpNo = function(){return this.empNo;}
+Employee.prototype.getEmpName = function(){return this.empName;}
+Employee.prototype.getEmpAge = function(){return this.empAge;}
+Employee.prototype.getEmpSal = function(){return this.empSal;}
+Employee.prototype.getEmpSex = function(){return this.empSex;}
+Employee.prototype.getEmpPart = function(){return this.empPart;}
+Employee.prototype.getEmpPos = function(){return this.empPos;}
+Employee.prototype.getEmpWorkHour = function(){return this.empWorkHour;}
 
+Employee.prototype.setEmpNo = function(empNo){this.empNo = empNo;}
+Employee.prototype.setEmpName = function(empName){this.empName = empName;}
+Employee.prototype.setEmpAge = function(empAge){this.empAge = empAge;}
+Employee.prototype.setEmpSal = function(empSal){this.empSal = empSal;}
+Employee.prototype.setEmpSex = function(empSex){this.empSex = empSex;}
+Employee.prototype.setEmpPart = function(empPart){this.empPart = empPart;}
+Employee.prototype.setEmpPos = function(empPos){this.empPos = empPos;}
+Employee.prototype.setEmpWorkHour = function(empWorkHour){this.empWorkHour = empWorkHour;}
