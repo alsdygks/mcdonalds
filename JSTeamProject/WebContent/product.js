a43b5f0e56a221928c2d482fa56b3057586a0513
Tacademy <Tacademy@192.168.205.178>
2017-07-25 18:58:12 +0900

    gd

--- JSTeamProject/WebContent/product.js
+++ JSTeamProject/WebContent/product.js
@@ -2,32 +2,52 @@
  * 
  */
 function Product(pNum,pName ,pPrice,pType,pWeight,pCalory){
-this.pNum=pNum,//제품코드
-this.pName =pName ,//제품명
-this.pPrice=pPrice, //제품가격
-this.pType=pType,//제품종류 (1버거,2사이드메뉴,3음료 )
-this.pWeight=pWeight, //제품 중량
-this.pCalory=pCalory //칼로리
+	this.pNum=pNum,//제품코드
+	this.pName =pName ,//제품명
+	this.pPrice=pPrice, //제품가격
+	this.pType=pType,//제품종류 (1버거,2사이드메뉴,3음료 )
+	this.pWeight=pWeight, //제품 중량
+	this.pCalory=pCalory //칼로리
 }
-Employee.prototype.getNo = function(){
+Employee.prototype.getpNum = function(){
 	return this.pNum
-	}
-
-Employee.prototype.getName = function(){
+}
+Employee.prototype.getpName = function(){
 	return this.pName
-	}
-Employee.prototype.getPrice = function(){
+}
+Employee.prototype.getpPrice = function(){
 	return this.pPrice
-	}
-Employee.prototype.getType = function(){
+}
+Employee.prototype.getpType = function(){
 	return this.pType
-	}
-Employee.prototype.getWeight = function(){
+}
+Employee.prototype.getpWeight = function(){
 	return this.pWeight
-	}
-Employee.prototype.getCalory = function(){
+}
+Employee.prototype.getpCalory = function(){
 	return this.pCalory
-	}
+}
+// 위  get 아래  set 
+Employee.prototype.setpNum = function(pNum){
+	this.pNum=pNum;
+}
+Employee.prototype.setpName = function(pName){
+	this.pName=pName;
+}
+Employee.prototype.setpPrice = function(pPrice){
+	this.pPrice=pPrice;
+}
+Employee.prototype.setpType = function(pType){
+	this.pType=pType;
+}
+Employee.prototype.setpWeight = function(pWeight){
+	this.pWeight=pWeight;
+}
+Employee.prototype.setpCalory = function(pCalory){
+	this.pCalory=pCalory;
+}
+
+
 
 
 
