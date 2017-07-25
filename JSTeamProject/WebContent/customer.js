d69a65f2163e4309d6bdf965c3abc9a0ef8e24bc
Tacademy <Tacademy@192.168.205.178>
2017-07-25 18:26:17 +0900

    이요한

--- JSTeamProject/WebContent/customer.js
+++ JSTeamProject/WebContent/customer.js
@@ -1,3 +1,12 @@
 /**
  * 
  */
+
+
+function Customer(cNum,cName,cGender,cAge){
+this.cNum=cNum,//고객 넘버
+this.cName=cName,//고객 이름
+this.cGender=cGender,//고객 성별
+this.cAge=cAge// 고객 연령대
+
+}
--- JSTeamProject/WebContent/order.js
+++ JSTeamProject/WebContent/order.js
@@ -1,3 +1,15 @@
 /**
  * 
  */
+function Order(odrNo,odrPNum,odrPName,odrEmpName,odrCNum,odrQuan,odrPrice,odrStore){
+
+this.odrNo=odrNo,//주문번호
+this.odrPNum=odrPNum,//상품코드
+this.odrPName=odrPName,//상품명
+this.odrEmpName=odrEmpName,//계산원
+this.odrCNum=odrCNum,//고객넘버
+this.odrQuan=odrQuan,//수량
+this.odrPrice=odrPrice,//가격
+this.odrStore=odrStore//판매점보
+
+}
