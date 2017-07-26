91c2c4e9b1496e62d79e5f0d7a46b1a584cf70b6
jinlee10 <jinleexjinlee@gmail.com>
2017-07-26 18:59:43 +0900

    커스토머 일단은 완성

--- JSTeamProject/WebContent/main/Customer.js
+++ JSTeamProject/WebContent/main/Customer.js
@@ -1,10 +1,9 @@
-function Customer(cNum,cName,cGender,cAge){
-this.cNum=cNum,//고객 넘버
-this.cName=cName,//고객 이름
-this.cGender=cGender,//고객 성별
-this.cAge=cAge//고객 연령대
-
+function Customer(cNum,cName,cAge,cSex,cWeight){
+	Person.call(this, cNum, cName,cAge,cSex)
+	this.cWeight = cWeight
 }
+
+Customer.prototype = new Person();
 
 //getters n setters
 Customer.prototype.getcNum = function(){return this.cNum;}//고객 넘버
@@ -16,6 +15,22 @@ Customer.prototype.setcNumo = function(cNum){this.cNum = cNum;}//고객 넘버
 Customer.prototype.setcName = function(cName){this.cName = cName;}//고객 이름
 Customer.prototype.setcGender = function(cGender){this.cGender = cGender;}//고객 성별
 Customer.prototype.setcAge = function(cAge){this.cAge = cAge;}//고객 연령대
+
+//기능
+Customer.prototype.purchase = function(){
+	console.log(this.name + '이(가) 결제하였습니다.');
+}
+Customer.prototype.order = function(){
+	console.log(this.name + '이(가) 음식을 주문하였습니다.');
+}
+Customer.prototype.eat = function(){
+	console.log(this.name + '이(가) 음식을 먹습니다.');
+	this.cWeight += 0.5;
+	console.log(this.name + '이(가) 음식을 먹어 몸무게가 늘었습니다.' + 
+			'현재 몸무게: ' + this.cWeight);
+}
+
+
 
 //toString
 Customer.prototype.toString = function(){
