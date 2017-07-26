6e5ea96e91099daeb09e7323b1c9f2a15d7bacb6
Tacademy <Tacademy@192.168.205.191>
2017-07-26 18:17:57 +0900

    Worker get set toString 생성

--- JSTeamProject/WebContent/Worker.js
+++ JSTeamProject/WebContent/Worker.js
@@ -1,3 +1,34 @@
 /**
  * 
  */
+
+function Worker(part){
+	this.part = part;
+}
+
+Worker.prototype.getPart = function(){
+	return this.part;
+};
+Worker.prototype.setPart = function(part){
+	this.part=part;
+}
+
+Worker.prototype.takeOrder = function(){
+	console.log('테이크 아웃을 합니다.');
+};
+
+Worker.prototype.bake = function(){
+	
+	console.log('햄버거를 만듭니다.');
+};
+Worker.prototype.cleanse = function(){
+	console.log('설거지를 합니다. ');
+
+};
+Worker.prototype.ride= function(){
+	console.log('배달을 합니다.');
+
+};
+Worker.prototype.toString = function(){
+	console.log('part : '+part);
+}
