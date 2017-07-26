4057288e01f18f21a5b2d8687975bdd0ad8af009
jinlee10 <jinleexjinlee@gmail.com>
2017-07-26 16:50:15 +0900

    aa

--- JSTeamProject/WebContent/run.js
+++ JSTeamProject/WebContent/run.js
@@ -1,3 +1,10 @@
 /**
  * 
  */
+
+var emp = [];
+//empNo, empName, empAge, empSex, empSal, empPart, empPos, empWorkHour
+emp.push(new Employee(1, 'John Cena', 55, true, 5000, '라이더', '평사원', 2));
+
+console.log(emp[0]);
+console.log(emp[0].toString());
