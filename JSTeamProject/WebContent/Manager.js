12b55098425fc5d6c71b8fbd19655bd20e5d83cd
minggry <minggry@DESKTOP-V6STGEL>
2017-07-26 18:18:53 +0900

    매니저

--- JSTeamProject/WebContent/Manager.js
+++ JSTeamProject/WebContent/Manager.js
@@ -1,3 +1,23 @@
-/**
- * 
- */    
+function Manager(mngNo,empNo){
+this.mngNo = mngNo,//매니저 넘버
+this.empNo = empNo//직원 넘버
+}
+
+//getters n setters
+Manager.prototype.getmngNo = function(){return this.mngNo;}//매니저 넘버
+Manager.prototype.getempNo = function(){return this.empNo;}//직원 넘버
+
+Manager.prototype.setmngNo = function(mngNo){this.mngNo = mngNo;}//매니저 넘버
+Manager.prototype.setempNo = function(empNo){this.empNo = empNo;}//직원 넘버
+
+//toString
+Manager.prototype.toString = function(){
+    return '매니저 넘버' + this.mngNo + ', 직원 넘버: ' + this.empNo;
+}
+
+Manager.prototype.hire = function(){
+	console.log('직원을 고용합니다');
+}
+Manager.prototype.mngWork = function(){
+	console.log('매니저 일을 합니다');
+}
