0d839668282d5553596d276e32edf68b49003b54
jinlee10 <jinleexjinlee@gmail.com>
2017-07-26 19:26:21 +0900

    run test

--- JSTeamProject/WebContent/main/Run.js
+++ JSTeamProject/WebContent/main/Run.js
@@ -1,3 +1,10 @@
 /**
  * 
  */
+
+//test
+/*function Owner(empNo, empName, empAge, empSex, 
+		empSal, empWorkHour,division){*/
+var owner = new Owner('o001', 'John Cena', 87, '남자', 3000, 3, '티아카데미');
+
+console.log(owner.toString());
