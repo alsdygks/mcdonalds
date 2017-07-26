b46dfb793de10d31103205c9623391af4d76edc5
minggry <minggry@DESKTOP-V6STGEL>
2017-07-26 17:32:25 +0900

    수정 변경전  이후로 밀어버림

--- JSTeamProject/WebContent/Product.js
+++ JSTeamProject/WebContent/Product.js
@@ -6,7 +6,7 @@ this.pNum=pNum,//제품코드
 this.pName =pName ,//제품명
 this.pPrice=pPrice, //제품가격
 this.pType=pType,//제품종류 (1버거,2사이드메뉴,3음료 )
-this.pWeight=pWeight, //제품 중량
+this.pWeight=pWeight, //제품 중량 
 this.pCalory=pCalory //칼로리
 }
 Employee.prototype.getNo = function(){
