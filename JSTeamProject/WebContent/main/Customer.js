a47cbe404e4c87e37fa9f818f4700cf26a3bd4b2
jinlee10 <jinleexjinlee@gmail.com>
2017-07-27 09:18:23 +0900

    고객-화장실 메소드 만듦

--- JSTeamProject/WebContent/main/Customer.js
+++ JSTeamProject/WebContent/main/Customer.js
@@ -23,6 +23,13 @@ Customer.prototype.eat = function(){
 	console.log(this.name + '이(가) 음식을 먹어 몸무게가 늘었습니다.' + 
 			'현재 몸무게: ' + this.cWeight);
 }
+Customer.prototype.pee = function(){
+	console.log(this.name + ' 고객님의 현재 몸무게: ' + this.cWeight);
+	console.log(this.name + ' 고객님이 화장실로 달려갔습니다.');
+	this.cWeight -= 1;
+	console.log(this.name + ' 고객님이 화장실에 다녀와 몸무게가 줄었습니다.' + 
+				'현재 몸무게: ' + this.cWeight);
+}
 
 
 //toString
